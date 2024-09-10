77de180764dbfd10027a0b05d3246603ca5b458c
Hevanafa <dhevansafa4@hotmail.com>
2024-09-10 11:32:34 +0700

    added basic square movement

--- main_unit.pas
+++ main_unit.pas
@@ -3,6 +3,8 @@ unit main_unit;
 {$mode ObjFPC}
 {$H+}
 {$m+}  // directive for constructors
+// https://forum.lazarus.freepascal.org/index.php?topic=38639.0
+{$COPERATORS ON}  // enables C operators like += and -=
 
 interface
 
@@ -53,6 +55,12 @@ begin
 
   last_t := now;
 
+  // init rectangle
+  sdlRect.x := 50;
+  sdlRect.y := 50;
+  sdlRect.w := 10;
+  sdlRect.h := 10;
+
   while not done do
   begin
     // writeln(dt);  // debug delta_t
@@ -77,17 +85,35 @@ begin
   readln;
 end;
 
+
 procedure TGameClass.update();
 begin
   SDL_PumpEvents;
 
   if sdlKeyboardState[SDL_SCANCODE_ESCAPE] = 1 then
     done := true;
+
+  if sdlKeyboardState[SDL_SCANCODE_W] = 1 then
+    sdlRect.y -= 1;
+  if sdlKeyboardState[SDL_SCANCODE_S] = 1 then
+    sdlRect.y += 1;
+
+  if sdlKeyboardState[SDL_SCANCODE_A] = 1 then
+    sdlRect.x -= 1;
+  if sdlKeyboardState[SDL_SCANCODE_D] = 1 then
+    sdlRect.x += 1;
 end;
+
 
 procedure TGameClass.draw();
 begin
+  // clear screen
+  SDL_SetRenderDrawColor(sdlRenderer, 100, 149, 237, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(sdlRenderer);
 
+  // draw the little black square
+  SDL_SetRenderDrawColor(sdlRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+  SDL_RenderDrawRect(sdlRenderer, @sdlRect);
 
   SDL_RenderPresent(sdlRenderer);
 end;
